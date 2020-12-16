bbe8733136f8decdd9ed23dc444cf44c200470fd
Shunze Lin <4A490101@stust.edu.tw>
2020-12-16 12:00:20 +0800

    Styling initial Header and Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import {Container} from 'react-bootstrap';
+import Header from './layout/Header';
+import Footer from './layout/Footer';
 
 const App = ()=> {
   return (
     <div className="App">
-      
+      <Header />
+      <main className = "py-3">
+      <Container>        
+        <h1>Welcome</h1>
+      </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
